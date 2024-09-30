0f972890dba43590ec76374c51d0ee4a390909e2
Raul Resendez <raul.resendez@proton.me>
2024-09-30 02:22:26 -0500

    have top row of grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-container = document.querySelector(".container");
+const container = document.querySelector(".container");
+const row = document.createElement("div");
+for (let i = 0; i<16; i++)
+    {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+
